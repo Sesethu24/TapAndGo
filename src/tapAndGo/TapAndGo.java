dfdd1be66d28fe2e04b4062bef3fde8697856cd9
Sesethu <seshmomoza20@gmail.com>
2021-05-03 12:56:48 +0200

    added a space

--- src/tapAndGo/TapAndGo.java
+++ src/tapAndGo/TapAndGo.java
@@ -4,30 +4,27 @@ public class TapAndGo {
 
     double balance;
 
-    public void pay(double amount){
-
-
+    public void pay(double amount)
+    {
         if (amount > balance)
         {
-
             System.out.println("You have insufficient funds for this purchase!" );
-        } else
-        {
-            balance = balance - amount;
-            System.out.println("You just used your card to pay" +"R"+ amount);
         }
+        else
+          {
+            balance = balance - amount;
+            System.out.println("You just used your card to pay" + " "+"R"+ amount);
+          }
     }
-    public void deposit(double amountDeposit){
-
+    public void deposit(double amountDeposit)
+    {
          balance = amountDeposit + balance;
          System.out.println( "R" + balance + " has been deposited into your account");
     }
-    public void getBalance(){
-
-        System.out.println("Your balance is" + " " + "R" + balance)
-
-        ;
-    }
+        public void getBalance()
+        {
+            System.out.println("Your balance is" + " " + "R" + balance);
+        }
 
 
 }
